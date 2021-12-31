4a5651faa160bbe7845ab6cb6ff0b7694bf2a8f5
rizwan alam <riscogian@gmail.com>
2021-12-30 21:45:55 -0800

    Add files via upload

---
+++ consumer (1).c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+
+#define SYS_reader 449
+int main()
+{
+	char* arr;
+	arr=(char*) malloc(8*sizeof(char));		
+	printf("RECIEVING\n");
+	long res = syscall(SYS_reader,arr);
+	printf("SYSTEM CALL RETURNED %ld.\n",res);
+	printf("RECIEVED %s\n",arr);
+	return 0;
+}
